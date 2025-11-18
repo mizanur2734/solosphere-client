530a9f7cb653ef8008c25e91970b5198cf4847f5
mizanur2734 <mizanurbb2791@gmail.com>
2025-11-18 19:49:07 +0600

    react project setup and create a navbar

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOVZE-9YpzBrpogWp5v9TC49rjgtm_jk0",
+  authDomain: "solosphere-c22ce.firebaseapp.com",
+  projectId: "solosphere-c22ce",
+  storageBucket: "solosphere-c22ce.firebasestorage.app",
+  messagingSenderId: "525195263042",
+  appId: "1:525195263042:web:662874e0ad4414173f41db"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <Navbar></Navbar>
+            {/* Outlet */}
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Main;
