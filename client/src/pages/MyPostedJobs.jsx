fc547c4ab60e6727498f3631b8d5c42911c60367
mizanur2734 <mizanurbb2791@gmail.com>
2025-11-25 18:40:07 +0600

    added the my post jobs delete method

--- client/src/pages/MyPostedJobs.jsx
+++ client/src/pages/MyPostedJobs.jsx
@@ -1,21 +1,35 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const MyPostedJobs = () => {
   const { user } = useContext(AuthContext);
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    const getData = async () => {
+    getData();
+  }, []);
+
+   const getData = async () => {
       const { data } = await axios(
         `${import.meta.env.VITE_API_URL}/jobs/${user?.email}`
       );
       setJobs(data);
     };
-    getData();
-  }, []);
-  console.log(jobs);
+  
+  const handleDelete = async id => {
+    try {
+      const { data } = await axios.delete(`${import.meta.env.VITE_API_URL}/job/${id}`);
+      console.log(data);
+      toast.success("Job posted successfully !");
+      // refresh ui
+      getData()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
   return (
     <section className="container px-4 mx-auto pt-12">
       <div className="flex items-center gap-x-3">
@@ -108,7 +122,7 @@ const MyPostedJobs = () => {
                       </td>
                       <td className="px-4 py-4 text-sm whitespace-nowrap">
                         <div className="flex items-center gap-x-6">
-                          <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
+                          <button onClick={() => handleDelete(job._id)} className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
--- server/index.js
+++ server/index.js
@@ -69,6 +69,15 @@ async function run() {
 
     }) 
 
+    // delete a job data form db
+    app.delete("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
+
+    }) 
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
