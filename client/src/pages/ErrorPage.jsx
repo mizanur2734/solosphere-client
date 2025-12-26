22006cb81d206daae2a7445223f996d7f6bca8ae
mizanur2734 <mizanurbb2791@gmail.com>
2025-12-26 21:23:47 +0600

    error page handle

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <section className="bg-white ">
+    <section className="bg-white">
       <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
         <div className="wf-ull lg:w-1/2">
           <p className="text-sm font-medium text-gray-500">404 error</p>
           <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
-            Page not found
+            Page not found 
           </h1>
+          
           <p className="mt-4 text-gray-500 dark:text-gray-400">
             Sorry, the page you are looking for doesnt exist.Here are some
             helpful links:
@@ -32,7 +33,7 @@ const ErrorPage = () => {
               </svg>
 
               <span>Go back</span>
-              
+
             </button>
 
             <Link to={"/"} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-gray-500 rounded-lg shrink-0 sm:w-auto hover:bg-gray-600">
